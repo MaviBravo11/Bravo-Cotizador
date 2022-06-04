c10c9227077fee0305c5a25def08f4289414fe51
Mavi Bravo <mavi.bravo@andromedalatam.com>
2022-06-04 14:53:18 -0300

    Primer entrega

---
+++ cotizaciones.js
@@ -0,0 +1,40 @@
+// // --------******** funcion cotizador --------********--------- 
+
+
+
+function cotizar() {
+
+    debugger
+    let pesos = parseInt(prompt('ingrese monto'));
+    
+    while (isNaN(pesos)) {
+        alert('Por favor ingrese un número')
+        pesos = parseInt(prompt('ingrese el monto en números'))
+    }
+
+    let moneda = prompt('ingrese moneda que necesita: DOLAR - EURO - REAL').toLowerCase()
+
+    switch (moneda) {
+        case "dolar":
+            return pesos / 210;
+            break;
+        case "euro":
+            return pesos / 250;
+            break;
+        case "real":
+            return pesos / 40;
+            break;
+        default:
+            alert('No pude cotizar lo que ingresaste');
+            break;
+    }
+   
+
+}
+alert("De la moneda elegida obtendrás: " + cotizar());
+
+
+
+
+
+
